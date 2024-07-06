bf98d60a137a95d9f4e3478671c66c0073356e13
decode6 <amitkumar696945@gmail.com>
2024-07-06 21:52:24 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Hero from './components/hero/Hero'
 
 const App = () => {
   return (
     <>
      <Navbar/>
+     <Hero/>
+     <About/>
 
     </>
   )
