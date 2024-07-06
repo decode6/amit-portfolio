e524cda5e8da1050a1e1aede89477c4beb4bcd1c
decode6 <amitkumar696945@gmail.com>
2024-07-07 00:05:42 +0530

    hero-name added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,10 +3,20 @@ import './hero.css';
 import hii from '../../assets/hii.png';
 const Hero = () => {
   return (
-    <div className='hero-name'>
+    <div className="hero-container">
+        <div className="hero">
+         <div className='hero-text'>
         <img src={hii} alt="" />
+        <div className="hero-name">
+            <p>Hi, I'm</p>
+            <h1>Amit</h1>
+        </div>
     
     </div>
+    </div> 
+    </div>
+   
+   
   )
 }
 
