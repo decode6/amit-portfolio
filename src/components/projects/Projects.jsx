3888cf1a4e7a48064039ca31fd16f953201701ff
decode6 <amitkumar696945@gmail.com>
2024-07-11 20:54:38 +0530

    project-category

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,6 +6,17 @@ const Projects = () => {
            <div className="project-text">
             <h1>My Creative Projects Section</h1>
            </div>
+           <div className="project-category">
+            <ul>
+              <li>All</li>
+              <li>Full-Stack</li>
+              <li>React</li>
+              <li>Mern-Stack</li>
+              <li>Python</li>
+              <li>HTML&CSS</li>
+              <li>JavaScript</li>
+            </ul>
+           </div>
     </div>
   )
 }
