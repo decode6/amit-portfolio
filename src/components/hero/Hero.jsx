ed4f8f150fd251aeab1ac2b55dded3c39dd74648
decode6 <amitkumar696945@gmail.com>
2024-07-23 17:20:10 +0530

    save

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,26 +5,26 @@ import hero_image from '../../assets/hero-img.png';
 const Hero = () => {
   return (
     <div className="hero-container">
-        <div className="hero">
-         <div className='hero-text'>
-        <img src={hii} alt="" />
-        <div className="hero-name">
+      <div className="hero">
+        <div className='hero-text'>
+          <img src={hii} alt="" />
+          <div className="hero-name">
             <p>Hi, I'm</p>
             <h1>Amit</h1>
+          </div>
+          <div className="header-role">
+            <h1>Software Engineer</h1>
+            <p>Full Stack Developer</p>
+          </div>
+
         </div>
-        <div className="header-role">
-          <h1>Software Engineer</h1>
-          <p>Full Stack Developer</p>
+        <div className="hero-img">
+          <img src={hero_image} alt="" />
         </div>
-    
+      </div>
     </div>
-    <div className="hero-img">
-      <img src={hero_image} alt="" />
-    </div>
-    </div> 
-    </div>
-   
-   
+
+
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,9 +3,9 @@ import './project.css'
 const Projects = () => {
 
   const cardData = [
-    { title: 'Card 1', description: 'This is the description for card 1.' },
-    { title: 'Card 2', description: 'This is the description for card 2.' },
-    { title: 'Card 3', description: 'This is the description for card 3.',link: "www.google.com" },
+    { title: 'Card 1', description: 'This is the description for card 1.' ,link: "https://www.google.com"},
+    { title: 'Card 2', description: 'This is the description for card 2.',link: "https://www.google.com" },
+    { title: 'Card 3', description: 'This is the description for card 3.',link: "https://www.google.com" },
   ];
 
 
@@ -14,7 +14,7 @@ const Projects = () => {
     <div className='project-section'>
            <div className="project-text">
             <h1>My Creative Projects Section</h1>
-           </div>
+           </div>   
            <div className="project-category">
             <ul>
               <li>All</li>
@@ -29,10 +29,10 @@ const Projects = () => {
            <div className="project-card">
        
     {cardData.map((data, index) => (
-            <div className="project">
-              key={index} {data.title} description={data.description} 
-            </div>
-         
+            <a href={data.link} key={index} className="project">
+               {data.title} description={data.description} 
+            </a>
+                    
       ))}
          
        
