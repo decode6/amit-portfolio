158316fbcace3bf4711ab026e4442e8a4948cc91
decode6 <amitkumar696945@gmail.com>
2024-07-23 22:16:37 +0530

    save

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,10 @@ const Skills = () => {
     <div className='skill-section'>
 <div className="skill-text">
   <h1>My Awesome Skills & Experience</h1>
-  <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in constant pursuit of skills that can help develop me into a well rounded professional.</p>
+  <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in <br /> constant pursuit of skills that can help develop me into a well rounded professional.</p>
+</div>
+<div className="skills-icon">
+  <img src="" alt="" />
 </div>
     </div>
   )
