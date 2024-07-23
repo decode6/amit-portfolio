fbb2efff9b0b85df2ba2e4a5f3fe4d4d6f3defb8
decode6 <amitkumar696945@gmail.com>
2024-07-23 20:14:34 +0530

    skill-section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import About from './components/about/About'
 import Hero from './components/hero/Hero'
 import Projects from './components/projects/Projects'
+import Skills from './components/skills/Skills'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
      <Hero/>
      <About/>
      <Projects/>
+     <Skills/>
 
     </>
   )
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import './skills.css'
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='skill-section'>
+<div className="skill-text">
+  <h1>My Awesome Skills & Experience</h1>
+  <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in constant pursuit of skills that can help develop me into a well rounded professional.</p>
+</div>
+    </div>
   )
 }
 
