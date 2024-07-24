abb37673bf50d9d27ac6623a585892c6c28f07dd
decode6 <amitkumar696945@gmail.com>
2024-07-24 20:46:28 +0530

    role added

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,19 +4,36 @@ import html from '../../assets/html-logo.png'
 const Skills = () => {
   return (
     <div className='skill-section'>
-<div className="skill-text">
-  <h1>My Awesome Skills & Experience</h1>
-  <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in <br /> constant pursuit of skills that can help develop me into a well rounded professional.</p>
-</div>
-<div className="skills-icon-section">
-  <div className="skill-icons">
-     <img src={html} alt="" />
-  </div>
-  <div className="skill-icons">
-     <img src={html} alt="" />
-  </div>
- 
-</div>
+      <div className="skill-text">
+        <h1>My Awesome Skills & Experience</h1>
+        <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in <br /> constant pursuit of skills that can help develop me into a well rounded professional.</p>
+      </div>
+      <div className="both-section">
+        <div className="skills-icon-section">
+          <div className="skill-icons">
+            <img src={html} alt="" />
+          </div>
+          <div className="skill-icons">
+            <img src={html} alt="" />
+          </div>
+
+        </div>
+
+        <div className="experience-section">
+          <div className="experience">
+            <h1>2023</h1>
+            <div className="job-role">
+               <h2>Full Stack Developer</h2>
+            <p>Risewings Technologies</p>
+            <p>June 2023 - Present</p>
+            </div>
+           
+          </div>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
