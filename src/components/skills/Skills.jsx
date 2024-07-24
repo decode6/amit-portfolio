471b27f13cedf0d69e8ce4417bf61f1f22165fc5
decode6 <amitkumar696945@gmail.com>
2024-07-24 19:18:51 +0530

    icons-added

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './skills.css'
+import html from '../../assets/html-logo.png'
 const Skills = () => {
   return (
     <div className='skill-section'>
@@ -7,8 +8,14 @@ const Skills = () => {
   <h1>My Awesome Skills & Experience</h1>
   <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in <br /> constant pursuit of skills that can help develop me into a well rounded professional.</p>
 </div>
-<div className="skills-icon">
-  <img src="" alt="" />
+<div className="skills-icon-section">
+  <div className="skill-icons">
+     <img src={html} alt="" />
+  </div>
+  <div className="skill-icons">
+     <img src={html} alt="" />
+  </div>
+ 
 </div>
     </div>
   )
