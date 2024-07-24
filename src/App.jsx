662558ee08a46c2df6d81d2dc8271c7939b95d56
decode6 <amitkumar696945@gmail.com>
2024-07-24 17:55:21 +0530

    assets added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import About from './components/about/About'
 import Hero from './components/hero/Hero'
 import Projects from './components/projects/Projects'
 import Skills from './components/skills/Skills'
+import Social from './components/social/Social'
 
 const App = () => {
   return (
     <>
+    
      <Navbar/>
      <Hero/>
      <About/>
      <Projects/>
      <Skills/>
-
+<Social/>
     </>
   )
 }
