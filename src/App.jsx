96de9420501757c17e1e658ce0fd510ed20da45d
decode6 <amitkumar696945@gmail.com>
2024-07-24 21:21:47 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import About from './components/about/About'
 import Hero from './components/hero/Hero'
 import Projects from './components/projects/Projects'
 import Skills from './components/skills/Skills'
-import Social from './components/social/Social'
+import Layout from './components/social/Layout'
 
 const App = () => {
   return (
     <>
-   
+   <Layout>
      <Navbar/>
      <Hero/>
      <About/>
      <Projects/>
      <Skills/>
- <Social/>
+ </Layout>
     </>
   )
 }
--- src/components/social/Social.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './social.css'
-const Social = () => {
-  return (
-    <div>Social</div>
-  )
-}
-
-export default Social
