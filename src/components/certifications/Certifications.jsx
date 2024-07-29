35ca1fafff828117859988377e687102e2a4b7f5
decode6 <amitkumar696945@gmail.com>
2024-07-29 18:32:51 +0530

    education

--- src/components/certifications/Certifications.jsx
+++ src/components/certifications/Certifications.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import './certifications.css'
 const Certifications = () => {
   return (
-    <div>Certifications
-      
+    <div className='certifications-section'>
+      <div className="cert-text">
+        <h1>My Education & Certifications</h1>
+      </div>
+      <div className="cert-main">
+        <div className="education">
+          <p>2021 - 2023</p>
+          <p>Master of Computer Applications <br />R V College of Engineering,Bengaluru, Karnataka</p><br />
+
+          <p> 2017 - 2021 <br />Bachelor of Computer Applications</p>
+          <p>Marwari College Bhagalpur, Bihar</p>
+          <br />
+
+          <p>Mar 2010 (Class XII) </p>
+          <p>+2 Boys High School, Sabour, Bhagalpur. <br />Bihar School Examination Board,</p><br />
+
+          <p>Mar 2010 (Class X) </p>
+          <p>+2 Boys High School, Sabour, Bhagalpur. <br />Bihar School Examination Board,</p>
+        </div>
+      </div>
     </div>
   )
 }
