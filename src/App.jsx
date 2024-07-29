34de1de0330cbf165daf5d3820bc5221f7cc1398
decode6 <amitkumar696945@gmail.com>
2024-07-29 22:54:59 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import About from './components/about/About'
-import Hero from './components/hero/Hero'
-import Projects from './components/projects/Projects'
-import Skills from './components/skills/Skills'
-import Layout from './components/social/Layout'
-import Certifications from './components/certifications/Certifications'
-import Contact from './components/contact/Contact'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
+import Hero from './components/hero/Hero';
+import Projects from './components/projects/Projects';
+import Skills from './components/skills/Skills';
+import Layout from './components/social/Layout';
+import Certifications from './components/certifications/Certifications';
+import Contact from './components/contact/Contact';
 
 const App = () => {
   return (
-    <>
-  
-      <Layout>
-        <Navbar />
-        <Hero />
-        <About />
-        <Projects />
-        <Skills />
-        <Certifications/>
-        <Contact/>
-      </Layout> 
-    </>
-  )
+    <Layout>
+      <Navbar />
+      <Routes>
+        <Route path='/hero' element={<Hero />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/certifications' element={<Certifications />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
       <h1 className='logo-icon'>Amit</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Skills</li>
-        <li>Certifications</li>
-        <li>Contact</li>
+      
+       
+        <ul> 
+          <li>Home</li>
+        <li><Link to="/hero">Hero</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
+        <li><Link to="/certifications">Certifications</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
+    
       <div></div>
     </nav>
   )
