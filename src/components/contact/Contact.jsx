fc7ed87dd602f6077eaa3c7c2b7fe47d4a9b3b3e
decode6 <amitkumar696945@gmail.com>
2024-07-29 20:08:10 +0530

    input

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,12 +5,22 @@ const Contact = () => {
   return (
     <div className='contact-section'>
       <div className="contact-text">
-        <h1>Connect with me</h1>
+        <h1> <span>Connect</span> with me</h1>
       </div>
       <div className="messenger">
           <div className="gmail">
             <img src={gmail} alt="" />
+            <p>amitkumar696945@gmail.com</p>
           </div>
+          <div className="gmail">
+            <img src={gmail} alt="" />
+            <p>amitkumar696945@gmail.com</p>
+          </div>
+      </div>
+      <div className="input">
+        <input type="text" />
+        <input type="email" />
+        <input type="" />
       </div>
     </div>
   )
