5abeb7c303fda7d15e30137ae38d5e50b942b602
decode6 <amitkumar696945@gmail.com>
2024-07-29 21:04:47 +0530

    commit

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <div className='skill-section'>
       <div className="skill-text">
-        <h1>My Awesome Skills & Experience</h1>
+        <h1>My Awesome <span>Skills</span> & <span>Experience</span></h1>
         <p>My skills are wideranging, spanning across multiple creative and technical disciplines. I absorb information like a sponge and I'm in <br /> constant pursuit of skills that can help develop me into a well rounded professional.</p>
       </div>
       <div className="both-section">
