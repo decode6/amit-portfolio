e6ceb666212f4b0926498d85539c30c8557ba4d5
decode6 <amitkumar696945@gmail.com>
2024-08-15 11:03:28 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+require('dotenv').config();
+const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log('Server is running on port 3000');
